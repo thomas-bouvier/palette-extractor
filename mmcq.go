75a6b6ac660d966badf8988f5752255f1c20f974
Thomas Bouvier <tomatrocho@gmail.com>
2018-05-27 22:51:55 +0200

    Added VBox object

--- mmcq.go
+++ mmcq.go
@@ -8,6 +8,21 @@ import (
 const BITSIG = 5
 const RSHIFT = 8 - BITSIG
 
+type VBox struct {
+	r1, r2    int
+	g1, g2    int
+	b1, b2    int
+	histogram map[int]int
+}
+
+func (vbox *VBox) volume() int {
+	sub_r := vbox.r2 - vbox.r1
+	sub_g := vbox.g2 - vbox.g1
+	sub_b := vbox.b2 - vbox.b1
+
+	return (sub_r + 1) * (sub_g + 1) * (sub_b + 1)
+}
+
 func quantize(pixels []Pixel, count int) {
 	if count < 2 || count > 256 {
 		fmt.Fprintf(os.Stderr, "wrong number of max colors when quantize")
@@ -20,6 +35,8 @@ func quantize(pixels []Pixel, count int) {
 		fmt.Fprintf(os.Stderr, "insufficient number of levels of quantification")
 		os.Exit(1)
 	}
+
+	fmt.Print(computeVBox(pixels, histogram))
 }
 
 func computeHistogram(pixels []Pixel) map[int]int {
@@ -39,6 +56,44 @@ func computeHistogram(pixels []Pixel) map[int]int {
 	return histogram
 }
 
+func computeVBox(pixels []Pixel, histogram map[int]int) VBox {
+	rmin := 1000000
+	rmax := 0
+	gmin := 1000000
+	gmax := 0
+	bmin := 1000000
+	bmax := 0
+
+	for _, pixel := range pixels {
+		r := pixel.R >> RSHIFT
+		g := pixel.G >> RSHIFT
+		b := pixel.B >> RSHIFT
+
+		rmin = min(r, rmin)
+		rmax = max(r, rmax)
+		gmin = min(g, gmin)
+		gmax = max(g, gmax)
+		bmin = min(b, bmin)
+		bmax = max(b, bmax)
+	}
+
+	return VBox{r1: rmin, r2: rmax, g1: gmin, g2: gmax, b1: bmin, b2: bmax, histogram: histogram}
+}
+
 func getColorIndex(r int, g int, b int) int {
 	return r<<(BITSIG*2) + g<<BITSIG + b
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
