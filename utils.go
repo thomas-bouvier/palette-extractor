58597386d319b5715e2e634ea52f9578d4aaeac0
Thomas Bouvier <tomatrocho@gmail.com>
2018-06-10 22:03:30 +0200

    Added color map object

--- utils.go
+++ utils.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 type Pixel struct {
 	R int
 	G int
@@ -28,6 +30,11 @@ type VBoxes struct {
 	sortingStrategy SortingStrategy
 }
 
+type CMap struct {
+	vboxes *VBoxes
+	colors []Pixel
+}
+
 func (vbox *VBox) volume() int {
 	sub_r := vbox.r2 - vbox.r1
 	sub_g := vbox.g2 - vbox.g1
@@ -50,6 +57,14 @@ func (vbox *VBox) Count() int {
 	}
 
 	return n
+}
+
+func (vbox *VBox) Contains(pixel *Pixel) bool {
+	r := pixel.R >> RSHIFT
+	g := pixel.G >> RSHIFT
+	b := pixel.B >> RSHIFT
+
+	return r >= vbox.r1 && r <= vbox.r2 && g >= vbox.g1 && g <= vbox.g2 && b >= vbox.b1 && b <= vbox.b2
 }
 
 func (vboxes VBoxes) Len() int {
@@ -87,6 +102,41 @@ func (vboxes *VBoxes) Push(x interface{}) {
 	item := x.(*VBox)
 	item.index = n
 	(*vboxes).boxes = append((*vboxes).boxes, item)
+}
+
+func (cmap *CMap) Map(color Pixel) *Pixel {
+	for i := 0; i < cmap.vboxes.Len(); i++ {
+		vbox := cmap.vboxes.boxes[i]
+
+		if vbox.Contains(&color) {
+			return &cmap.colors[i]
+		}
+	}
+
+	return cmap.nearest(color)
+}
+
+func (cmap *CMap) nearest(color Pixel) *Pixel {
+	d1 := -10000.0
+	var ret *Pixel
+
+	for i := 0; i < cmap.vboxes.Len(); i++ {
+		d2 := math.Sqrt(
+			math.Pow(float64(color.R-cmap.colors[i].R), 2) +
+				math.Pow(float64(color.G-cmap.colors[i].G), 2) +
+				math.Pow(float64(color.B-cmap.colors[i].B), 2))
+
+		if d2 < d1 {
+			d1 = d2
+			ret = &cmap.colors[i]
+		}
+	}
+
+	return ret
+}
+
+func (cmap *CMap) Len() int {
+	return cmap.vboxes.Len()
 }
 
 func min(x, y int) int {
