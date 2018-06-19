03e9b36c5dfa705b07b965c56ec356213200c470
Thomas Bouvier <tomatrocho@gmail.com>
2018-06-19 22:34:41 +0200

    Added main object

--- extractor.go
+++ extractor.go
@@ -7,25 +7,40 @@ import (
 	"os"
 )
 
-func main() {
+type Extractor struct {
+	filename string
+	quality  int
+	pixels   []Pixel
+}
+
+func NewExtractor(filename string, quality int) *Extractor {
+	extractor := &Extractor{}
+
 	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
 
-	reader, err := os.Open("./image.png")
+	reader, err := os.Open(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 	}
-
 	defer reader.Close()
 
-	pixels, err := getPixels(reader, 10)
+	pixels, err := getPixels(reader, quality)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s", err)
 		os.Exit(1)
 	}
 
-	fmt.Print(getPalette(pixels, 5))
+	extractor.filename = filename
+	extractor.quality = quality
+	extractor.pixels = pixels
+
+	return extractor
 }
 
-func getPalette(pixels []Pixel, count int) []Pixel {
-	return quantize(pixels, count).GetPalette()
+func (extractor *Extractor) GetPalette(count int) []Pixel {
+	return quantize(extractor.pixels, count).GetPalette()
+}
+
+func (extractor *Extractor) GetColor() Pixel {
+	return extractor.GetPalette(5)[0]
 }
